a9c807ef19d0cde2e848fa954d2e14d36f0528d4
Dream-Diver-Co <dreamdiver2017@gmail.com>
2024-09-12 17:45:03 +0600

    Login Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,12 @@ import Courses from "./Courses/Courses.jsx";
 function App() {
   return (
     <>
+      <div className="dark:bg-slate-900 dark:text-white">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Course" element={<Courses />} />
       </Routes>
+      </div>
     </>
   );
 }
--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import list from "../../public/list.json";
 import Card from "./Card";
 
@@ -19,9 +20,11 @@ function Course() {
             ut fugiat officia libero dolorem, sed quos saepe inventore tempora
             adipisci.
           </p>
+          <Link to = '/'>
           <button className="mt-6 bg-pink-500 rounded-md text-white hover:bg-pink-700 px-4 py-2">
             Back
           </button>
+          </Link>
           <div className="mt-12 grid grid-cols-1 md:grid-cols-3 ml-12 mr-12">
             {list.map((item) => (
               <Card key={item.id} item={item} />
