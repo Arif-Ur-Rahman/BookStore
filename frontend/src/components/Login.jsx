472fae6a049ae7b82cfda7dae8c5e6ef11fbb851
Arif Ur Rahman <arifurrahman.it.doc@gmail.com>
2024-09-12 22:54:08 +0600

    Updated Client site Form Validation from frontend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
   return (
     <>
       <div>
         <dialog id="my_modal_3" className="modal">
           <div className="modal-box">
-            <form method="dialog">
+            <form onSubmit={handleSubmit(onSubmit)} method="dialog">
               {/* if there is a button in form, it will close the modal */}
               <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
                 ✕
               </button>
-            </form>
+            
             <h3 className="font-bold text-lg">Login</h3>
             <div className="mt-4 space-y-2">
               <span>Email</span>
@@ -21,7 +32,10 @@ function Login() {
                 name="email"
                 placeholder="Enter Your Email"
                 className="w-80 px-3 py-2 border rounded-md outline-none"
+                {...register("email", { required: true })}
               />
+              <br />
+              {errors.email && <span className="text-red-500 text-sm">This field is required</span>}
             </div>
 
             {/* Pass */}
@@ -33,7 +47,11 @@ function Login() {
                 name="email"
                 placeholder="Password"
                 className="w-80 px-3 py-2 border rounded-md outline-none"
+                {...register("password", { required: true })}
+                
               />
+              <br />
+              {errors.password && <span className="text-red-500 text-sm">This field is required</span>}
             </div>
             <div className="flex justify-around mt-4">
               <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
@@ -41,9 +59,10 @@ function Login() {
               </button>
               <p>
                 Not Registered? {""}
-                <span className="underline text-blue-500 cursor-pointer">Sign Up</span>{""}
+                <Link to='/Signup' className="underline text-blue-500 cursor-pointer">Sign Up</Link>{""}
               </p>
             </div>
+            </form>
           </div>
         </dialog>
       </div>
